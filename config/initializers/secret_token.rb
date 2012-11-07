9b062f7ee70824bdce8a407a5c07628259b9be44
Bruno Santos Chagas <brunoprimo@gmail.com>
2012-11-07 17:59:57 -0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Webredonda::Application.config.secret_token = '34538a2ab5ff1e4f0291f653d5e4ef6e164c8db6ba42dc8405179223e70d4e68acf6e314854c980386090bce36e81a3e6dad91e5874018da6ea59ac462b6aa11'
